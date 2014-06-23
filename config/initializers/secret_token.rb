d32b12bd12ba7ecd11e4cd389a3c1eed0d7d8831
KimShin <jcynico@gmail.com>
2014-06-24 00:35:22 +0900

    Why Rails?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mall::Application.config.secret_key_base = 'dd32f5081d7c5800500601c69b4b35ede8505e35d9b4e7083d61a6938a2326fdb5dda33d143aee60768ab6883e22c47cd48d343a34dd00ab223a1026971a54d8'
